2fc2aac0d3afee9a45a0e4c8faff8332c91ab87a
Hairon <korhary@gmail.com>
2026-02-02 21:03:17 +0900

    Update uvicorn server

--- nlp_engine/server.py
+++ nlp_engine/server.py
@@ -1,22 +1,11 @@
 from fastapi import FastAPI, Body, HTTPException
-from engine import NLPEngine
-
-# FIXME: Use for Server test
-from tests.helloserver_test import helloServer
-
-#Server TEST
-hello_server = helloServer()
+from .engine import NLPEngine
 
 #The main NLP engine
 engine = NLPEngine()
 
 # Run http://server-path/
 app = FastAPI()
-
-@app.post("/hello/{msg}")
-def hello_api(msg: str):
-    result = hello_server.server(msg)
-    return {"message": result}
 
 @app.post("/nlpengine/")
 async def nlpengine(data: dict = Body(...)):
